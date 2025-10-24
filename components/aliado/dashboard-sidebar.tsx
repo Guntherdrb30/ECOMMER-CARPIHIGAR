f5d973e18c07f5638c2475770aae02ab1cc0bc10
carpihogarai <carpihogar.ai@gmail.com>
2025-10-24 10:27:37 -0700

    feat(ally-sidebar): add top CTA button - Nuevo Presupuesto

--- components/aliado/dashboard-sidebar.tsx
+++ components/aliado/dashboard-sidebar.tsx
@@ -25,6 +25,7 @@ const icons: { [key: string]: React.ReactNode } = {
   Heart: <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor"><path fillRule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" clipRule="evenodd" /></svg>,
   MapPin: <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor"><path fillRule="evenodd" d="M5.05 4.05a7 7 0 119.9 9.9L10 20l-4.95-5.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z" clipRule="evenodd" /></svg>,
   LogOut: <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor"><path fillRule="evenodd" d="M3 3a1 1 0 00-1 1v12a1 1 0 102 0V5h10a1 1 0 100-2H3zm11.707 4.707a1 1 0 00-1.414-1.414L10 9.586 6.707 6.293a1 1 0 00-1.414 1.414L8.586 11l-3.293 3.293a1 1 0 101.414 1.414L10 12.414l3.293 3.293a1 1 0 001.414-1.414L11.414 10l3.293-3.293z" clipRule="evenodd" /></svg>,
+  Plus: <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" viewBox="0 0 20 20" fill="currentColor"><path fillRule="evenodd" d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z" clipRule="evenodd"/></svg>,
 };
 
 export function AllyDashboardSidebar() {
@@ -32,6 +33,13 @@ export function AllyDashboardSidebar() {
   return (
     <aside className="w-64 flex-shrink-0 border-r border-gray-200 bg-gray-50 p-4">
       <nav className="flex flex-col space-y-2">
+        <Link
+          href="/dashboard/aliado/presupuestos/nuevo"
+          className="inline-flex items-center justify-center gap-2 px-3 py-2 rounded-lg text-sm font-semibold text-white bg-blue-600 hover:bg-blue-700"
+        >
+          {icons['Plus']} <span>Nuevo Presupuesto</span>
+        </Link>
+        <hr className="my-2" />
         {navItems.map((item) => {
           const isActive = pathname === item.href;
           return (
